37131a6bf0d60878a2500e557712aea0d1962295
Cosmin Vancea <73666215+csvancea@users.noreply.github.com>
2021-05-01 02:07:25 +0300

    Read Wi-Fi/server configuration from SD Card code refactor

--- fire-alarm.ino
+++ fire-alarm.ino
@@ -1,51 +1,133 @@
+#include <SPI.h>
+#include <SD.h>
+#include <IniFile.h>
+
 #include "src/ESP8266.h"
 
-ESP8266 WiFi(3, 2);
+#define PIN_ESP8266_RX 3
+#define PIN_ESP8266_TX 4
+
+#define SLEEP_FOREVER() while(1) { delay(~0); }
+
+ESP8266 WiFi(PIN_ESP8266_RX, PIN_ESP8266_TX);
+
+String WiFi_SSID, WiFi_Pass;
+String Server_Host, Server_GUID;
+unsigned short Server_Port;
+
+void InitSDCard()
+{
+	char buff[64];
+
+	Serial.print("Initializing SDCard ...");
+	pinMode(PIN_SPI_SS, OUTPUT);
+	if (!SD.begin(PIN_SPI_SS)) {
+		goto fail;
+	}
+	Serial.println(" OK!");
+
+	
+	Serial.print("Reading configuration file ...");
+
+	IniFile config("/FIRE-A~1/NET-CO~1.INI", FILE_READ);
+	if (!config.open()) {
+		goto fail;
+	}
+
+	if (!config.getValue("wifi", "ssid", buff, sizeof(buff))) {
+		goto fail;
+	}
+	WiFi_SSID = buff;
+	
+	if (!config.getValue("wifi", "pass", buff, sizeof(buff))) {
+		goto fail;
+	}
+	WiFi_Pass = buff;
+
+	if (!config.getValue("server", "host", buff, sizeof(buff))) {
+		goto fail;
+	}
+	Server_Host = buff;
+
+	if (!config.getValue("server", "port", buff, sizeof(buff))) {
+		goto fail;
+	}
+
+	Server_Port = strtoul(buff, NULL, 10);
+	if (!config.getValue("server", "guid", buff, sizeof(buff))) {
+		goto fail;
+	}
+	Server_GUID = buff;
+
+	Serial.println(" OK!");
+
+	config.close();
+	SD.end();
+	return;
+
+fail:
+	Serial.println(" FAILED!");
+	SLEEP_FOREVER();
+}
+
+void InitWiFi()
+{
+	Serial.print("Waiting for Wi-Fi ...");
+
+	/* Wait until module is communicating */
+	while (!WiFi.IsInitialized()) {
+		delay(1000);
+	}
+
+	Serial.println(" OK!");
+}
+
+void TestWiFiConnection()
+{
+	Serial.print("Testing Wi-Fi connection ...");
+
+	if (!WiFi.ConnectToAP(WiFi_SSID, WiFi_Pass)) {
+		goto fail;
+	}
+
+	if (!WiFi.StartConnection("TCP", Server_Host, Server_Port)) {
+		goto fail;
+	}
+
+	if (!WiFi.Send("ARDUINO + ESP8266 = <3")) {
+		goto fail;
+	}
+
+	if (!WiFi.CloseConnection()) {
+		goto fail;
+	}
+
+	if (!WiFi.DisconnectFromAP()) {
+		goto fail;
+	}
+
+	Serial.println(" OK!");
+	return;
+
+fail:
+	Serial.println(" FAILED!");
+	SLEEP_FOREVER();
+}
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(9600);
-	Serial.println("BOOT");
-
-	delay(2000);
-
-	while (!WiFi.IsInitialized()) {
+	while (!Serial) {
 		delay(1000);
 	}
 
-	while (!WiFi.Reset()) {
-		delay(1000);
-	}
+	Serial.println("\n-------- BOOT --------\n");
 
-	while (!WiFi.IsInitialized()) {
-		delay(1000);
-	}
+	InitSDCard();
+	InitWiFi();
+	TestWiFiConnection();
 
-	while (!WiFi.SetMode(CWMODE_STATION)) {
-		delay(1000);
-	}
-
-	while (!WiFi.ConnectToAP("SSID", "PASSWORD")) {
-		delay(1000);
-	}
-
-	while (!WiFi.StartConnection("TCP", "HOST", 7331)) {
-		delay(1000);
-	}
-
-	while (!WiFi.Send("ARDUINO + ESP8266 = <3")) {
-		delay(1000);
-	}
-
-	while (!WiFi.CloseConnection()) {
-		delay(1000);
-	}
-
-	while (!WiFi.DisconnectFromAP()) {
-		delay(1000);
-	}
-
-	Serial.println("WiFi Test OK!");
+	Serial.println("\n-------- DONE --------\n");
 }
 
 // the loop function runs over and over again until power down or reset
